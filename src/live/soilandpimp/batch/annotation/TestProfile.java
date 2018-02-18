1c43c7422945b6a3192c28485e9e65c1c5217e17
NYPD <nypd54@outlook.com>
2018-02-18 15:16:47 -0500

    Remove excess anotations from the profiles annotations

--- src/live/soilandpimp/batch/annotation/TestProfile.java
+++ src/live/soilandpimp/batch/annotation/TestProfile.java
@@ -1,15 +1,8 @@
 package live.soilandpimp.batch.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import org.springframework.context.annotation.Profile;
 
 import live.soilandpimp.batch.util.AppConstants;
 
-@Target({ElementType.TYPE, ElementType.METHOD})
-@Retention(RetentionPolicy.RUNTIME)
 @Profile(AppConstants.TEST_PROFILE)
 public @interface TestProfile {}
