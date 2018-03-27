3f4685d5bc1370bfc9bb0c3a36318ebf8913b8f1
NYPD <nypd54@outlook.com>
2018-03-26 20:52:24 -0400

    Fix broken junit tests due to wonky annotations

--- src/live/soilandpimp/batch/annotation/DevelopmentProfile.java
+++ src/live/soilandpimp/batch/annotation/DevelopmentProfile.java
@@ -1,8 +1,15 @@
 package live.soilandpimp.batch.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import org.springframework.context.annotation.Profile;
 
 import live.soilandpimp.batch.util.AppConstants;
 
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 @Profile(AppConstants.DEV_PROFILE)
 public @interface DevelopmentProfile {}
