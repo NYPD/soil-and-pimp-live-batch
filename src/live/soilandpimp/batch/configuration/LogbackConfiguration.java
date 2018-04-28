c5bca48db03da4f575bce773af909e1bca759720
NYPD <nypd54@outlook.com>
2018-04-28 03:00:22 -0400

    Switch batching logging directory

--- src/live/soilandpimp/batch/configuration/LogbackConfiguration.java
+++ src/live/soilandpimp/batch/configuration/LogbackConfiguration.java
@@ -30,7 +30,7 @@ import live.soilandpimp.batch.util.AppConstants;
 public class LogbackConfiguration {
 
     private final String encoderPattern = "%d{[yyyy-MM-dd HH:mm:ss.SSS]} [%-5level]\\(%F{0}:%M\\(\\):%L\\) - %msg%n";
-    private final String filePattern = "/var/log/" + AppConstants.PROJECT_NAME + "/goddess.%d{yyyy-MM-dd}.log";
+    private final String filePattern = "/var/log/batch/" + AppConstants.PROJECT_NAME + "/goddess.%d{yyyy-MM-dd}.log";
     private final LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
 
     @Autowired
